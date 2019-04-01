0ae4e8fbd07313ff728c1d7335d4db1a1cfd850c
dmaiocchi <dmaiocchi@suse.com>
2019-04-01 18:56:21 +0200

    first draft

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject missile "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "rocketchat rest-http library"
+  :url "https://github.com/MalloZup/missile"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                         [clj-http "3.9.1"]]
   :main ^:skip-aot missile.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/missile/core.clj
+++ src/missile/core.clj
@@ -1,7 +1,31 @@
 (ns missile.core
-  (:gen-class))
+  (:require [clj-http.client :as client]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+
+  (defn init-client [server user pwd]
+    (let [server-api-endpoint (reduce str [server "/api/v1/login"]) ]
+    (println server-api-endpoint)
+    (client/post server-api-endpoint {:basic-auth [user pwd] :insecure? true} )))
+  
+  (defn read-config [config-file]
+    (slurp config-file))
+  
+  ;; make .missile.edn configurable for the path
+
+
+  (def config-map
+     (clojure.edn/read-string (read-config ".missile.edn")))
+  
+  (def get-rocket-user
+    (-> config-map :credentials :username))
+  
+  (def get-rocket-token
+    (-> config-map :credentials :token))
+  
+  (def get-rocket-server
+    (-> config-map :credentials :server-url))
+  
+  (defn -main
+    [& args]
+    (let [response (init-client get-rocket-server get-rocket-user get-rocket-token)] 
+      println (:status response)))
