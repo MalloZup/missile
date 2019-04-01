61347929d42c23105732f6aa32375fd0b5ff2aa9
dmaiocchi <dmaiocchi@suse.com>
2019-04-02 00:11:21 +0200

    remove debug info

--- src/missile/channels.clj
+++ src/missile/channels.clj
@@ -2,7 +2,7 @@
   (:require [clj-http.client :as client])
   (:require [missile.config :as config]))
 
-  ;; move this to channel namespace
   (defn list! []
+    "list rocket-chat channels"
     (let [server-api-endpoint (reduce str [config/server "/api/v1/channels.list"]) ]
-    (client/get server-api-endpoint { :headers {"X-Auth-Token" config/token "X-User-Id" config/user} :debug true :debug-body true} )))
+    (client/get server-api-endpoint { :headers {"X-Auth-Token" config/token "X-User-Id" config/user}} )))
