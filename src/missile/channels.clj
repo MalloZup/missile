f3459e17451467904ebb22d28e1ebbbeba112b41
dmaiocchi <dmaiocchi@suse.com>
2019-04-02 14:55:46 +0200

    split body

--- src/missile/channels.clj
+++ src/missile/channels.clj
@@ -2,7 +2,14 @@
   (:require [clj-http.client :as client])
   (:require [missile.config :as config]))
 
-  (defn list! []
+  ;; internal/private
+  (defn get-list-channels []
     "list rocket-chat channels"
     (let [server-api-endpoint (reduce str [(config/server) "/api/v1/channels.list"]) ]
     (client/get server-api-endpoint { :headers {"X-Auth-Token" (config/token) "X-User-Id" (config/user)}} )))
+
+  ;; public api
+   ;; TODO: convert to json object
+  (defn list! []
+  "return a list of maps containining channel list"
+    (:body (get-list-channels)))
