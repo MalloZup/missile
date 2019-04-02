0d3b43a335ace4a2d61cd1a96cd4fa1b7fcfb47e
dmaiocchi <dmaiocchi@suse.com>
2019-04-02 18:13:45 +0200

    add json output

--- src/missile/channels.clj
+++ src/missile/channels.clj
@@ -1,15 +1,15 @@
 (ns missile.channels
   (:require [clj-http.client :as client])
-  (:require [missile.config :as config]))
+  (:require [missile.config :as config])
+  (:require [clojure.data.json :as json]))
 
   ;; internal/private
   (defn get-list-channels []
-    "list rocket-chat channels"
+    "return raw list rocket-chat channels, as defined by clj-http schema"
     (let [server-api-endpoint (reduce str [(config/server) "/api/v1/channels.list"]) ]
     (client/get server-api-endpoint { :headers {"X-Auth-Token" (config/token) "X-User-Id" (config/user)}} )))
 
   ;; public api
-   ;; TODO: convert to json object
   (defn list! []
-  "return a list of maps containining channel list"
-    (:body (get-list-channels)))
+  "return a  json object, list of maps containining channel list"
+    (json/read-str (:body (get-list-channels))))
