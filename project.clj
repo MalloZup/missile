750824016addd6d48e923689e777939281e564a8
dmaiocchi <dmaiocchi@suse.com>
2019-04-06 00:00:09 +0200

    add cheesire json and make send-msg call

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
+                 [cheshire "5.8.1"]
                  [clj-http "3.9.1"]
                  [org.clojure/data.json "0.2.6"]]
   :main ^:skip-aot missile.core
